af0c7842294d716c1eabcc739174c4af2e357cb8
theogirl <theogirl@outlook.com>
2016-05-23 17:12:00 -0400

    plastic cups

--- prod-eco-paper-cups.php
+++ prod-eco-paper-cups.php
@@ -2,15 +2,15 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="The Cup Store is your premier source for custom printed disposable paper and plastic cups, glassware, containers, coffee sleeves and much more. Grow your brand and your business one cup at a time - print your logo or other artwork on a cup!">
   <meta name="keywords" content="paper cups, plastic cups, glassware, coffee sleeves, gelato cups, ice cream cups, compostable cups, biodegradeable cups, eco cups, printed paper cups, printed plastic cups, custom printed cups, imprinted paper cups, imprinted plastic cups, promotional cups, logo on cups, custom cups, customized cups">
-    
+
     <title>Eco-Paper Cups | The Cup Store</title>
 
  <?php include 'includes/head.php'; ?>
- 
+
 </head>
 
 <body>
@@ -30,7 +30,7 @@
 
 <!--FEATURETTE-->
 <div class="container">
-      <section class="row"> 
+      <section class="row">
        <div class="col-md-5">
           <img class="img-responsive" src="images/products/hero-paper-eco.jpg" alt="Eco-Paper Cup" title="Eco-Paper Cup">
         </div>
@@ -47,11 +47,12 @@
     </div><!--end featurette container-->
 
 
-<!--PRICING TABLES-->
-
 <div class="container">
 
 <!--Thumbnails: Cups-->
+<div class="row-pp">
+
+    <h3 class="text-center">All sizes come in white only. Minimum order = 100 cups.</h3>
     <section class="row">
             <div class="col-xs-6 col-sm-2 tcs-product">
             <img src="images/products/paper-eco-4oz.jpg" alt="Eco-paper-cup-4oz" title="Eco-paper-cup-4oz">
@@ -83,109 +84,13 @@
             <h4>20 oz.</h4>
             </div>
     </section><!--end cup thumbnails-->
+</div>
 
-    <h3 class="text-center">All sizes come in white only. Minimum order = 100 cups.</h3>
-
-
-<!--Price Table: Cups-->
-<section class="panel panel-default">
-  <div class="panel-heading">Prices <strong>per cup</strong>, in U.S. Dollars <strong>($USD)</strong></div>
-    <div class="panel-body">
-       <p class="hidden-sm hidden-md hidden-lg tabletip">Touch table and scroll to see pricing <i class="fa fa-arrow-right"></i></p>
-      <div class="table-responsive">
-        <table class="table table-striped tcs-table">
-          <thead>
-            <tr>
-              <th>Item No.</th>
-              <th>Description</th>
-              <th>100+</th>
-              <th>250+</th>
-              <th>500+</th>
-              <th>1,000</th>
-              <th>3,000+</th>
-              <th>5,000+</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <th>4P-ECO</th>
-              <th>4oz Compostable Paper Cup</th>
-              <th>$0.73</th>
-              <th>$0.60</th>
-              <th>$0.52</th>
-              <th>$0.34</th>
-              <th>$0.33</th>
-              <th>Call Us</th>
-            </tr>
-
-            <tr>
-              <th>8P-ECO</th>
-              <th>8oz Compostable Paper Cup</th>
-              <th>$0.78</th>
-              <th>$0.64</th>
-              <th>$0.59</th>
-              <th>$0.38</th>
-              <th>$0.36</th>
-              <th>Call Us</th>
-            </tr>
-
-            <tr>
-              <th>10P-ECO</th>
-              <th>10oz Compostable Paper Cup</th>
-              <th>$0.83</th>
-              <th>$0.68</th>
-              <th>$0.60</th>
-              <th>$0.39</th>
-              <th>$0.38</th>
-              <th>Call Us</th>
-            </tr>
-
-            <tr>
-              <th>12P-ECO</th>
-              <th>12oz Compostable Paper Cup</th>
-              <th>$0.85</th>
-              <th>$0.70</th>
-              <th>$0.65</th>
-              <th>$0.42</th>
-              <th>$0.39</th>
-              <th>Call Us</th>
-            </tr>
-            
-            <tr>
-              <th>16P-ECO</th>
-              <th>16oz Compostable Paper Cup</th>
-              <th>$1.02</th>
-              <th>$0.84</th>
-              <th>$0.69</th>
-              <th>$0.45</th>
-              <th>$0.42</th>
-              <th>Call Us</th>
-            </tr>
-
-            <tr>
-              <th>20P-ECO</th>
-              <th>20oz Compostable Paper Cup</th>
-              <th>$1.15</th>
-              <th>$0.94</th>
-              <th>$0.80</th>
-              <th>$0.52</th>
-              <th>$0.49</th>
-              <th>Call Us</th>
-            </tr>
-          </tbody>
-      </table>
-    </div><!--end table-responsive-->
-  </div><!--end panel body-->
- </section><!--end cup pricing tables-->
-
-<!--HIGH VOLUME DISCOUNT-->
-
-<?php include 'includes/volume.php'; ?>
-
- <!--LIDS-->
 
 <!--Thumbnails: Lids-->
+<div class="row-pp">
+<h3 class="text-center">You may also want to pair your cup with a matching lid. </h3>
+
 <section class="row">
   <div class="col-xs-6 col-sm-2 col-sm-offset-1 tcs-lid">
   <img src="images/products/lid-4P.jpg" alt="white vented lid" title="white vented lid">
@@ -212,124 +117,22 @@
   <h5>ECO-Dome</h5>
   </div>
 </section><!--end lid thumbnails-->
-
-<h3 class="text-center">You may also want to pair your cup with a matching lid. </h3>
-
-<!--Price Table: Lids-->
-<section class="panel panel-default">
-  <div class="panel-heading">Prices in U.S. Dollars <strong>($USD)</strong></div>
-    <div class="panel-body">
-        <p class="hidden-sm hidden-md hidden-lg tabletip">Touch table and scroll to see pricing <i class="fa fa-arrow-right"></i></p><!--to assist user on mobile only-->
-      <div class="table-responsive">
-        <table class="table table-striped tcs-table">
-
-          <thead>
-            <tr>
-              <th>Item No.</th>
-              <th>Description</th>
-              <th>Per 100</th>
-              <th>Per Case</th>
-              <th>Case Qty</th>
-            </tr>
-          </thead>
-
-          <tbody>
-             <tr>
-              <th>4P-Lid</th>
-              <th>White vented lid for 4oz paper cups</th>
-              <th>$4.00</th>
-              <th>$35.00</th>
-              <th>1,000</th>
-            </tr>
-
-            <tr>
-              <th>8P-Lid</th>
-              <th>White tear back lid for 8oz paper cups</th>
-              <th>$4.50</th>
-              <th>$39.00</th>
-              <th>1,000</th>
-            </tr>
-
-            <tr>
-              <th>8P-Dome</th>
-              <th>White dome lid for 8oz paper cups</th>
-              <th>$6.00</th>
-              <th>$50.00</th>
-              <th>1,000</th>
-            </tr>
-
-            <tr>
-              <th>8P-Dome Blk</th>
-              <th>Black dome lid for 8oz paper cups</th>
-              <th>$6.50</th>
-              <th>$57.00</th>
-              <th>1,000</th>
-            </tr>
-            
-            <tr>
-              <th>Tear-Lid</th>
-              <th>White tear back lid for 10/12/16/20/24oz paper cups</th>
-              <th>$5.00</th>
-              <th>$45.00</th>
-              <th>1,000</th>
-            </tr>
-
-            <tr>
-              <th>Dome-Lid</th>
-              <th>White dome lid for 10/12/16/20/24oz paper cups</th>
-              <th>$6.00</th>
-              <th>$52.00</th>
-              <th>1,000</th>
-            </tr>
-
-            <tr>
-              <th>Dome-Lid Blk</th>
-              <th>Black dome lid for 10/12/16/20/24oz paper cups</th>
-              <th>$6.00</th>
-              <th>$52.00</th>
-              <th>1,000</th>
-            </tr>
-
-            <tr>
-              <th>ECO-Dome</th>
-              <th>ECO dome lid for 10/12/16/20/24oz paper cups</th>
-              <th>$16.00</th>
-              <th>$145.00</th>
-              <th>1,200</th>
-            </tr>
-
-          </tbody>
-
-        </table>
-      </div><!--end table-responsive-->
-      
-    </div><!--end panel body-->
-  </section><!--end lid pricing tables-->
-
-
-
-<!--<MULTI-ITEM DISCOUNT-->
-
-<?php include 'includes/multi-item.php'; ?>
+</div>
 
 </div><!--end container-->
-
-<!--JAVA PACKAGING-->
-
-<?php include 'includes/java.php'; ?>
 
 
 <!--FOOTER SECTION-->
 
 <?php include 'includes/footer.php'; ?>
-    
 
 
 
 
-    <!-- Latest compiled and minified JavaScript 
+
+    <!-- Latest compiled and minified JavaScript
     ================================================== -->
-	
+
 	<script type="text/javascript" src="js/jquery-1.11.2.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
--- prod-paper-cups.php
+++ prod-paper-cups.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Primary Printers is your premier source for custom printed disposable paper and plastic cups, glassware, containers, coffee sleeves and much more. Grow your brand and your business one cup at a time - print your logo or other artwork on a cup!">
   <meta name="keywords" content="paper cups, plastic cups, glassware, coffee sleeves, gelato cups, ice cream cups, compostable cups, biodegradeable cups, eco cups, printed paper cups, printed plastic cups, custom printed cups, imprinted paper cups, imprinted plastic cups, promotional cups, logo on cups, custom cups, customized cups">
-    
+
     <title>Paper Cups | Primary Printers</title>
 
 <?php include 'includes/head.php'; ?>
@@ -30,7 +30,7 @@
 <!--FEATURETTE-->
 <div class="container">
       <section class="row">
-      
+
        <div class="col-md-5">
           <img class="img-responsive" src="images/products/hero-paper-std.jpg" alt="Standard Paper Cup">
         </div>
@@ -43,9 +43,9 @@
           <p class="cuplead-sm">Our paper cups are made from a high quality white paperboard that provides the ideal surface for a clear custom imprint to market your brand. They are lined on the inside, which makes them ideal for hot beverages. Our cup is made out of fiber that comes from responsibly managed forests and meets the Sustainable Forestry Initiative&#174; (SFI&#174;) standards and are 100% recyclable.</p>
           <p>Our cups can be paired with either a dome sip lid, available in white or black, or a flat lid. They lids are universal, as well – you can fit multiple cup sizes, all with just one lid.</p>
           <hr>
-    
+
           <p><i class="fa fa-arrow-circle-right fa-lg"></i> <a href="docs/pp-paper-cups-pricing.pdf" class="pricing">View pricing info for standard paper cups and lids [PDF]</a></p>
-          <p>For complete information on how to plce your order, supply artwork, shipping and our terms and conditions, please refer to our complete <a href="docs/primaryprinters-catalog.pdf">Primary Printers catalog</a>.</p>
+          <p>For complete information on how to plce your order, supply artwork, shipping and our terms and conditions, please refer to our complete <a href="http://www.primaryprinters.com/PrimaryPrintersCatalog.pdf">Primary Printers catalog</a>.</p>
         </div>
         </div>
 
@@ -96,10 +96,6 @@
       <h4>20 oz.</h4>
       </div>
 
-      <!--<div class="col-xs-6 col-sm-2 tcs-product">
-      <img src="images/products/paper-std-24oz.jpg" alt="paper-cup-4oz" title="paper-cup-24oz">
-      <h4>24 oz.</h4>
-      </div>-->
     </section><!--end cup thumbnails-->
 </div>
 
@@ -110,7 +106,7 @@
 
 
 <div class="row-pp">
-<h3 class="text-center">You may also want to pair your cup with a matching lid. </h3> 
+<h3 class="text-center">You may also want to pair your cup with a matching lid. </h3>
 
 <section class="row">
 
@@ -147,9 +143,9 @@
 <?php include 'includes/footer.php'; ?>
 
 
-    <!-- Latest compiled and minified JavaScript 
+    <!-- Latest compiled and minified JavaScript
     ================================================== -->
-	
+
 	<script type="text/javascript" src="js/jquery-1.11.2.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 
